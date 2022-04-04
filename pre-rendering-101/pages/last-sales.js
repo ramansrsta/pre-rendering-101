01961607ca402d8abf7441de1511461f52b6a310
rmnsrs <ramanshrestha@eeposit.com>
2022-04-04 22:20:04 +0545

    adding get static props to client side

--- pre-rendering-101/pages/last-sales.js
+++ pre-rendering-101/pages/last-sales.js
@@ -1,14 +1,12 @@
 import { useEffect, useState } from "react";
 import useSwr from "swr";
 
-export default function LastSalesPage() {
+export default function LastSalesPage(props) {
 	const fetcher = (...args) => fetch(...args).then((res) => res.json());
 	const { data, error } = useSwr(
 		"https://jsonplaceholder.typicode.com/todos/",
 		fetcher
 	);
-
-	console.log(data, "data");
 
 	if (error) {
 		return <p> Failed To Load ...</p>;
@@ -18,6 +16,21 @@ export default function LastSalesPage() {
 	}
 
 	return (
-		<ul>{data && data?.map((todo) => <li key={todo.id}>{todo.title}</li>)}</ul>
+		<ul>
+			{data && props.data?.map((todo) => <li key={todo.id}>{todo.title}</li>)}
+		</ul>
 	);
+}
+
+export async function getStaticProps() {
+	return fetch("https://jsonplaceholder.typicode.com/todos/")
+		.then((response) => response.json())
+		.then((json) => {
+			return {
+				props: {
+					data: json,
+				},
+				revalidate: 10,
+			};
+		});
 }
